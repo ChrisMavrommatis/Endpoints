584180820705132fe17b62c302cd129a7c927a7d
Chris Mavrommatis <56303004+ChrisMavrommatis@users.noreply.github.com>
2024-07-24 19:38:27 +0300

    initial commit

---
+++ src/Endpoints/ExtensionMethods/TypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace ChrisMavrommatis.Endpoints.ExtensionMethods;
+
+internal static class TypeExtensions
+{
+    internal static IEnumerable<Type> GetBaseTypesAndThis(this Type type)
+    {
+        Type? current = type;
+        while (current != null)
+        {
+            yield return current;
+            current = current.BaseType;
+        }
+    }
+}
---
+++ src/Endpoints/Requests/RequestWithBody.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChrisMavrommatis.Endpoints.Requests;
+
+public abstract class RequestWithBody<TBody>
+{
+    [FromBody]
+    public TBody Body { get; set; }
+}
